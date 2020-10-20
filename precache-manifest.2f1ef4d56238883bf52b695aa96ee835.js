0d4dbef6862b8d9c8a6b5cab70ecc62a60c2d59a
Barry <mamadoubarry6059@gmail.com>
2020-10-20 15:01:25 +0000

    Updates

---
+++ precache-manifest.2f1ef4d56238883bf52b695aa96ee835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b831cc4a6cafa7772fb19c674c1880a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a49f57e8c05cbd066308",
+    "url": "/my-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "4faea498ef4403e7a719",
+    "url": "/my-app/static/css/main.117f8788.chunk.css"
+  },
+  {
+    "revision": "a49f57e8c05cbd066308",
+    "url": "/my-app/static/js/2.86bb8362.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-app/static/js/2.86bb8362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4faea498ef4403e7a719",
+    "url": "/my-app/static/js/main.75185c70.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
